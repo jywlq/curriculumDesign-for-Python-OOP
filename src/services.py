473d6cd30d9f1eeb7f878d2f3e329cd7321a0e49
Lucky <yggdya@163.com>
2026-07-08 16:12:04 +0800

    完成updatePerson编辑方法，设计了防止编号冲突和允许修改自身

--- src/services.py
+++ src/services.py
@@ -7,10 +7,22 @@ class personService:
     def __init__(self):
         self.personList=[]
 
-    def addPerson(self,person:baseClass):
+
+    def personIDcheck(self,personID:str):
+        '''
+        检查编号是否存在
+        '''
         for p in self.personList:
-            if p._personID==person._personID:
-                return False
+            if p._personID==personID:
+                return True
+        return False
+
+    def addPerson(self,person:baseClass):
+        '''
+        按类添加
+        '''
+        if self.personIDcheck(person._personID):
+            return False
         self.personList.append(person)
         return True
 
@@ -32,6 +44,20 @@ class personService:
                 self.personList.remove(p)
                 return True
         return False
+    
+    def updatePerson(self,personID:str,person:baseClass):
+        if self.personIDcheck(person._personID) and personID!=person._personID:
+            '''
+            双重检查，防止编号冲突并允许修改自身
+            '''
+            return False
+        for i,p in enumerate(self.personList):
+            if p._personID==personID:
+                self.personList[i]=person
+                return True
+        return False
+    
+    
     def save(self,filename:str='data/person.json'):
         '''
         由调用方决定何时保存，main层调用
@@ -40,11 +66,16 @@ class personService:
             json.dump([p.to_dict() for p in self.personList],
                       f,ensure_ascii=False,indent=4)
             
+            
+    '''
+    核心save和load方法，供外部调用  
+    '''
     def load(self,filename:str='data/person.json'):
         '''
         由调用方决定何时加载，main层调用
         '''
         classMap={c.__name__:c for c in [baseClass,teacher,experimenter,admin,teacher_admin]}
+        #classmap方法：将类名映射到类对象，便于从字典中恢复对象
         try:
             with open(filename,'r',encoding='utf-8') as f:
                 data_list=json.load(f)
@@ -59,7 +90,7 @@ class personService:
     test
     '''
     def __str__(self):
-        return str([str(p) for p in self.personList])
+        return ''.join([str(p)+'\n' for p in self.personList])
     
     '''
     testEnd
